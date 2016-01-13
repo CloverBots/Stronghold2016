42f9ffe9a283a608b1978127b6d16e43ad2e080d
Mackinnon Buck <mackinnonman@icloud.com>
2016-01-12 20:23:46 -0800

    Initial commit with robot code.
    Contains the base for what will be the Stronghold robot code.

---
+++ Stronghold2016/src/Robot.cpp
@@ -0,0 +1,65 @@
+#include "WPILib.h"
+#include "Commands/Command.h"
+#include "CommandBase.h"
+
+class Robot: public IterativeRobot
+{
+private:
+	std::unique_ptr<Command> autonomousCommand;
+	SendableChooser *chooser;
+
+	void RobotInit()
+	{
+		CommandBase::init();
+		chooser = new SendableChooser();
+		SmartDashboard::PutData("Autonomous Modes", chooser);
+	}
+
+	void DisabledInit()
+	{
+	}
+
+	void DisabledPeriodic()
+	{
+		Scheduler::GetInstance()->Run();
+	}
+
+	void AutonomousInit()
+	{
+		/* std::string autoSelected = SmartDashboard::GetString("Auto Selector", "Default");
+		if(autoSelected == "My Auto") {
+			autonomousCommand.reset(new MyAutoCommand());
+		} else {
+			autonomousCommand.reset(new ExampleCommand());
+		} */
+
+		autonomousCommand.reset((Command *)chooser->GetSelected());
+
+		if (autonomousCommand != NULL)
+			autonomousCommand->Start();
+	}
+
+	void AutonomousPeriodic()
+	{
+		Scheduler::GetInstance()->Run();
+	}
+
+	void TeleopInit()
+	{
+		if (autonomousCommand != NULL)
+			autonomousCommand->Cancel();
+	}
+
+	void TeleopPeriodic()
+	{
+		Scheduler::GetInstance()->Run();
+	}
+
+	void TestPeriodic()
+	{
+		LiveWindow::GetInstance()->Run();
+	}
+};
+
+START_ROBOT_CLASS(Robot)
+
